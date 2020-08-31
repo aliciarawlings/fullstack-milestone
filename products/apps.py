38502e5d02048ae7210b43fa968e35dc3513f140
aliciarawlings <lilyrawlings123@gmail.com>
2020-08-31 22:50:04 +0000

    admin tailored and products added and displaying

--- products/apps.py
+++ products/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
 
+class CategoryConfig(AppConfig):
+    name = 'category'
+
+
 class ProductsConfig(AppConfig):
-    name = 'products'
+    name = 'product'
--- products/models.py
+++ products/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural ='Categories'
+        
     flavour = models.CharField(max_length=250)
-    Dietary = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.flavour
 
 
 class Product(models.Model):
